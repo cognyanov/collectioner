5c5b90bad7af9cdbc7a21004714e27e8b4d79d13
cognyanov <79589328+cognyanov@users.noreply.github.com>
2021-04-07 15:39:16 +0300

    Add files via upload

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -1,7 +1,28 @@
 window.onload = () => {
-    let currEnergy = document.getElementById("currEnergy");
+    if (window.location.pathname !== '/admin') {
+        let currEnergy = document.getElementById("currEnergy");
 
-    console.log(currEnergy.textContent * 10 + "%");
-    document.getElementById("energyBar").style.width = currEnergy.textContent * 10 + "%";
 
+        document.getElementById("energyBar").style.width = currEnergy.textContent * 10 + "%";
+    }
+
+
+    if (window.location.pathname === '/admin') {
+        let promoteUserDiv = document.getElementById("divPromoteUser");
+        let demoteUserDiv = document.getElementById("divDemoteUser");
+
+
+        document.getElementById("promoteUserBtn").addEventListener("click", () => {
+            promoteUserDiv.style.display = "block";
+            demoteUserDiv.style.display = "none";
+
+        });
+
+        document.getElementById("demoteUserBtn").addEventListener("click", () => {
+            demoteUserDiv.style.display = "block";
+            promoteUserDiv.style.display = "none";
+
+        })
+
+    }
 };
